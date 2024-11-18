b73fd9060992044d0ece524768345e88db1a207b
chinaglia-rafa <chinaglia.rafa@gmail.com>
2024-11-18 09:55:20 -0300

    aula(seção-04): conclui aula 35 sobre useState

--- events-and-hooks/src/App.tsx
+++ events-and-hooks/src/App.tsx
@@ -1,15 +1,23 @@
-import { MouseEvent } from "react";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  const handleClick = (e: MouseEvent) => {
-    e.stopPropagation();
-    alert("botão");
+  // useState é responsável por representar o estado de um componente,
+  // e retorna seu valor e uma função para modificá-lo, permitindo
+  // que o DOM seja atualizado apenas onde é necessário ao ter seu
+  // valor modificado.
+  //
+  // O instrutor chamou de "memória do componente"
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
   };
 
   return (
-    <div onClickCapture={() => alert("cliquei na div")}>
-      <button onClick={handleClick}>Botão</button>
+    <div>
+      <h2>{count}</h2>
+      <button onClick={handleClick}>Add +1</button>
     </div>
   );
 };
